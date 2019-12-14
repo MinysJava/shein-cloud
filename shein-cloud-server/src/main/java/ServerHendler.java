d12e52c62abdf2528869c016837a3c77cf17a792
JavaProg <msh86.02.18@rambler.ru>
2019-12-14 21:19:57 +0500

    Зависимость в maven

---
+++ shein-cloud-server/src/main/java/ServerHendler.java
@@ -0,0 +1,17 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ServerHendler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        System.out.println("ping");
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
