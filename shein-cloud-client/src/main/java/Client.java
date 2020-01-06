30662559972f06b3e9c9f84103af67872eb50aa2
JavaProg <msh86.02.18@rambler.ru>
2020-01-06 18:21:11 +0500

    добавлено главное окно и окно авторизации

--- shein-cloud-client/src/main/java/Client.java
+++ shein-cloud-client/src/main/java/Client.java
@@ -1,38 +1,22 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Client {
+public class Client extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Shein_Cloud Client");
+        Scene scene = new Scene(root,400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        Network.start();
-
-
-//        Thread t = new Thread(() -> {
-            try {
-                Network.sendMsg(new FileMessage(Paths.get("client_file/redCircle.png" )));
-                Network.sendMsg(new Request("rename","poem_server.txt", "super_new_poem.txt"));
-                Network.sendMsg(new Request("send", "music.mp3"));
-                Network.sendMsg(new Request("delete", "del.txt"));
-
-                while (true) {
-                    AbstractMessage am = Network.readObject();
-                    if (am instanceof FileMessage) {
-                        FileMessage fm = (FileMessage) am;
-                        Files.write(Paths.get("client_file/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
-
-                    }
-                }
-            } catch (ClassNotFoundException | IOException e) {
-                e.printStackTrace();
-            } finally {
-                Network.stop();
-            }
-//        });
-//        t.setDaemon(true);
-//        t.start();
-
-
-
+        launch(args);
     }
 }
---
+++ shein-cloud-client/src/main/java/MainController.java
@@ -0,0 +1,53 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    ListView<String> filesListClient;
+    @FXML
+    ListView<String> filesListServer;
+    @FXML
+    ListView<String> textResultList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Network.start();
+
+
+//        Thread t = new Thread(() -> {
+        try {
+            Network.sendMsg(new FileMessage(Paths.get("client_file/redCircle.png" )));
+            Network.sendMsg(new Request("rename","poem_server.txt", "super_new_poem.txt"));
+            Network.sendMsg(new Request("send", "music.mp3"));
+            Network.sendMsg(new Request("delete", "del.txt"));
+
+            while (true) {
+                AbstractMessage am = Network.readObject();
+                if (am instanceof FileMessage) {
+                    FileMessage fm = (FileMessage) am;
+                    Files.write(Paths.get("client_file/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
+
+                }
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Network.stop();
+        }
+//        });
+//        t.setDaemon(true);
+//        t.start();
+
+
+
+    }
+}
